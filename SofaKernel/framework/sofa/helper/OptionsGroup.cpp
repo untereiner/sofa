b719ec14081c30ef5b490f120cf946b20f2fdd87
Bruno Marques <bruno.josue.marques@inria.fr>
2017-03-22 16:52:19 +0100

    FIX: allowing optionsgroup to have an-specified default value when initialized according to another data. ex: If an OptionsGroup needs to contain the list of files in a specific folder specified by a Data<std::string>, the OptionsGroup will be considered as empty at init, and thus the value written in the scene file will be ignored.

--- SofaKernel/framework/sofa/helper/OptionsGroup.cpp
+++ SofaKernel/framework/sofa/helper/OptionsGroup.cpp
@@ -1,24 +1,21 @@
 /******************************************************************************
 *       SOFA, Simulation Open-Framework Architecture, development version     *
-*                (c) 2006-2016 INRIA, USTL, UJF, CNRS, MGH                    *
+*                (c) 2006-2017 INRIA, USTL, UJF, CNRS, MGH                    *
 *                                                                             *
-* This library is free software; you can redistribute it and/or modify it     *
+* This program is free software; you can redistribute it and/or modify it     *
 * under the terms of the GNU Lesser General Public License as published by    *
 * the Free Software Foundation; either version 2.1 of the License, or (at     *
 * your option) any later version.                                             *
 *                                                                             *
-* This library is distributed in the hope that it will be useful, but WITHOUT *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
 * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
 * for more details.                                                           *
 *                                                                             *
 * You should have received a copy of the GNU Lesser General Public License    *
-* along with this library; if not, write to the Free Software Foundation,     *
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
 *******************************************************************************
-*                              SOFA :: Framework                              *
-*                                                                             *
-* Authors: The SOFA Team (see Authors.txt)                                    *
+* Authors: The SOFA Team and external contributors (see Authors.txt)          *
 *                                                                             *
 * Contact information: contact@sofa-framework.org                             *
 ******************************************************************************/
@@ -106,6 +103,14 @@ int OptionsGroup::isInOptionsList(const std::string & tempostring) const
     }
     return -1;
 }
+
+///////////////////////////////////////
+void OptionsGroup::setSelectedItemToDefault()
+{
+    setSelectedItem(xmlValue);
+}
+
+
 ///////////////////////////////////////
 void OptionsGroup::setSelectedItem(unsigned int id_item)
 {
@@ -133,11 +138,11 @@ unsigned int OptionsGroup::getSelectedId() const
 ///////////////////////////////////////
 const std::string& OptionsGroup::getSelectedItem() const
 {
-	if (textItems.empty())
-	{
-		static std::string empty;
-		return empty;
-	}
+    if (textItems.empty())
+    {
+        static std::string empty;
+        return empty;
+    }
     return textItems[selectedItem];
 }
 ///////////////////////////////////////
@@ -149,10 +154,13 @@ void OptionsGroup::readFromStream(std::istream & stream)
     if (id_stringinButtonList == -1)
     {
         int idx=atoi(tempostring.c_str());
-        if (idx >=0 && idx < (int)size()) 
-			setSelectedItem(idx);
+        if (idx >=0 && idx < (int)size())
+            setSelectedItem(idx);
         else
+        {
             msg_warning("OptionsGroup") << "\""<< tempostring <<"\" is not a parameter in button list :\" "<<(*this)<<"\"";
+            xmlValue = tempostring;
+        }
     }
     else
     {
